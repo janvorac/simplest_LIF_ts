d50c5bc7c82876784c94e440cd0d2b1c5ee352a3
Jan Vorac <vorac@mail.muni.cz>
2020-03-24 08:58:03 +0100

    creating plots

---
+++ src/plot.ts
@@ -0,0 +1,81 @@
+import {
+  select,
+  max,
+  min,
+  scaleLinear,
+  axisBottom,
+  axisLeft,
+  line,
+  Axis
+} from './d3.min.js'
+
+export default class plotMaker{
+  protected container: Selection<SVGGElement, null, null, null>;
+  protected plotGroup: Selection<SVGGElement, null, SVGElement, null>;
+  protected yAxis: Axis<number>;
+  protected xAxis: Axis<number>;
+  protected margin: object;
+  protected width: number;
+  protected height: number;
+
+  public constructor(laserProfData: Array<object>, fluoData: Array<object>){
+    this.margin = {
+      top: 10,
+      right: 30,
+      bottom: 40,
+      left: 60
+    };
+    this.container = select("#plot-container")
+    this.width = 500 - this.margin.left - this.margin.right;
+    this.height = 200 - this.margin.top - this.margin.bottom;
+
+    this.plotGroup = this.container.append("g")
+      .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`)
+
+    const timeValue = d => d.time;
+    const intensityValue = d => d.intensity;
+    const fluorescenceValue = d => d.fluorescence;
+
+    const xScale = scaleLinear()
+      .domain([min(laserProfData, timeValue), max(laserProfData, timeValue)])
+      .range([0, this.width]);
+
+    const yScale = scaleLinear()
+      .domain([min(laserProfData, intensityValue), max(laserProfData, intensityValue)])
+      .range([this.height, 0]);
+
+    const xAxis = this.plotGroup.append("g")
+      .call(axisBottom(xScale))
+      .attr("transform", `translate(0,${this.height})`)
+
+    const yAxis = this.plotGroup.append("g")
+      .call(axisLeft(yScale))
+
+    // Laser profile line
+    this.plotGroup.append("path")
+      .classed("chartLine", true)
+      .classed("laserProf", true)
+      .datum(laserProfData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("d", line()
+        .x(d => xScale(timeValue(d)))
+        .y(d => yScale(intensityValue(d)))
+      )
+
+    // Fluorescence line
+    this.plotGroup.append("path")
+      .classed("chartLine", true)
+      .classed("fluo", true)
+      .datum(fluoData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("d", line()
+        .x(d => xScale(timeValue(d)))
+        .y(d => yScale(intensityValue(d)))
+      )
+
+  }
+
+
+}
