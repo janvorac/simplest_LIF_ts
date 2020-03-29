2dc0e7870d7b03e93a0d0ddcce5d4d7cd6de1f9a
Jan Vorac <vorac@mail.muni.cz>
2020-03-29 10:33:10 +0200

    fluorescence opacity settings with slider works!

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import plotMaker from './plot'
 import Model from './model'
-import addImage from './add_image'
+import {
+  xml,
+  select
+} from 'd3';
 
 const model = new Model(0, 200)
 
 const pMaker = new plotMaker(model.data)
 
-addImage();
+xml("data/LIF.svg")
+  .then(data => {
+    select("div#svg-container").node().append(data.documentElement)
+    select("#laser-beam")
+      .style("opacity", 1)
+    select("#time-slider")
+      .on("input", setTime)
+  })
+
+function setTime(): void {
+  //const laserOpacity = Model.laserProfile(this.value)
+  const laserOpacity = model.data[this.value]["laserProf"]
+  select("#laser-beam")
+    .style("opacity", +laserOpacity)
+  const fluoOpacity = model.data[this.value]["fluorescence"]
+  select("#fluorescence")
+    .style("opacity", +fluoOpacity)
+}
